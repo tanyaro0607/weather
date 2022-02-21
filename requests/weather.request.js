a52e90da41098cdebf6e87ce038bce07dce7fcf7
tanyaro0607 <ta.rohlova@yandex.ru>
2022-02-21 21:21:18 +0300

    the weather app

---
+++ requests/weather.request.js
@@ -0,0 +1,36 @@
+const rp = require('request-promise')
+
+module.exports = async function(city = '') {
+  if (!city) {
+    throw new Error('Имя города не может быть пустым')
+  }
+
+  const KEY = '5f082bb3024cdf72972cce288ba76616'
+  const uri = 'http://api.openweathermap.org/data/2.5/weather'
+
+  const options = {
+    uri,
+    qs: {
+      appid: KEY,
+      q: city,
+      units: 'imperial'
+    },
+    json: true
+  }
+
+  try {
+    const data = await rp(options)
+    const celsius = (data.main.temp - 32) * 5/9
+
+    return {
+      weather: `${data.name}: ${celsius.toFixed(0)}`,
+      error: null
+    }
+  } catch (error) {
+    // console.log(error)
+    return {
+      weather: null,
+      error: error.error.massage
+    }
+  } 
+}
